be6d9a815e99263a653a1adff847e51ec7703459
Aijobhunter09 <avishroyal9@gmail.com>
2026-08-17 11:44:21 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cloudflare(),
-  ],
+  plugins: [react()],
 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-
-  optimizeDeps: {
-    exclude: ['@cloudflare/workers-types'],
   },
 });
